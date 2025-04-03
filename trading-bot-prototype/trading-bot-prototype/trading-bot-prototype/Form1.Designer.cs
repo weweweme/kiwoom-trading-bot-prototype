f81adcea038c890128ec0e464694155657465870
JINMACHINE\argen <argentdarae@gmail.com>
2025-04-04 00:42:20 +0900

    feat: UI 컴포넌트 배치

--- trading-bot-prototype/trading-bot-prototype/trading-bot-prototype/Form1.Designer.cs
+++ trading-bot-prototype/trading-bot-prototype/trading-bot-prototype/Form1.Designer.cs
@@ -29,13 +29,73 @@ namespace trading_bot_prototype
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axKHOpenAPI1 = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCheckConnect = new System.Windows.Forms.Button();
+            this.rtxtLog = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axKHOpenAPI1
+            // 
+            this.axKHOpenAPI1.Enabled = true;
+            this.axKHOpenAPI1.Location = new System.Drawing.Point(688, 388);
+            this.axKHOpenAPI1.Name = "axKHOpenAPI1";
+            this.axKHOpenAPI1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI1.OcxState")));
+            this.axKHOpenAPI1.Size = new System.Drawing.Size(100, 50);
+            this.axKHOpenAPI1.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Green;
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(12, 415);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "로그인";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnCheckConnect
+            // 
+            this.btnCheckConnect.Location = new System.Drawing.Point(93, 415);
+            this.btnCheckConnect.Name = "btnCheckConnect";
+            this.btnCheckConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckConnect.TabIndex = 2;
+            this.btnCheckConnect.Text = "연결 확인";
+            this.btnCheckConnect.UseVisualStyleBackColor = true;
+            // 
+            // rtxtLog
+            // 
+            this.rtxtLog.Location = new System.Drawing.Point(440, 12);
+            this.rtxtLog.Name = "rtxtLog";
+            this.rtxtLog.Size = new System.Drawing.Size(348, 360);
+            this.rtxtLog.TabIndex = 3;
+            this.rtxtLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtxtLog);
+            this.Controls.Add(this.btnCheckConnect);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.axKHOpenAPI1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCheckConnect;
+        private System.Windows.Forms.RichTextBox rtxtLog;
     }
 }
 
