eac99f6b6dfb6bc6ba6937db2dd93abdf7b34a4e
JINMACHINE\argen <argentdarae@gmail.com>
2025-04-04 22:08:57 +0900

    feat: AccountUIContext 클래스 구현

---
+++ trading-bot-prototype/trading-bot-prototype/trading-bot-prototype/AccountUIContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace trading_bot_prototype
+{
+    /// <summary>
+    /// AccountService에서 사용하는 UI 컨트롤들을 모은 구조체
+    /// </summary>
+    public class AccountUiContext
+    {
+        public ComboBox CmbAccounts { get; set; }
+        public Label LblUserId { get; set; }
+        public Label LblUserName { get; set; }
+        public Label LblServerType { get; set; }
+        public Label LblBalance { get; set; }
+    }
+}
