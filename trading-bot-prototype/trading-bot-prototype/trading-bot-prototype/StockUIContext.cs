13384c9aefafc5473935ad52db6d4a5717ff1fb6
JINMACHINE\argen <argentdarae@gmail.com>
2025-04-04 22:59:32 +0900

    feat: StockUIContext 구현

---
+++ trading-bot-prototype/trading-bot-prototype/trading-bot-prototype/StockUIContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace trading_bot_prototype
+{
+    /// <summary>
+    /// 종목 검색 및 조회 기능에서 사용하는 UI 컨트롤들을 묶는 컨텍스트
+    /// </summary>
+    public class StockUIContext
+    {
+        public TextBox TxtStockName { get; set; }              // 종목명 입력
+        public ListBox LstStockCandidates { get; set; }        // 후보 리스트
+        public TextBox TxtStockCode { get; set; }              // 종목코드 입력
+    }
+}
